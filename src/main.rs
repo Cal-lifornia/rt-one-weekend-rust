50533bffc10edd8113f1ca12b35d8d3fb027c891
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-05-26 16:00:24 +1000

    Created the first image

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
-use std::io;
-
-use color::Color;
-
-mod color;
-mod rays;
-mod vec3;
+const IMAGE_WIDTH: i32 = 256;
+const IMAGE_HEIGHT: i32 = 256;
 
 fn main() {
-    // Image dimensions
-    // Image
-
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: i32 = 400;
-    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-
-    // Render
-
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    for j in (0..IMAGE_HEIGHT).rev() {
-        eprintln!("\rScanlines remaining: {}", j);
-        for i in (0..IMAGE_WIDTH) {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let r: f32 = i as f32 / (IMAGE_WIDTH as f32 - 1.0);
+            let g: f32 = j as f32 / (IMAGE_HEIGHT as f32 - 1.0);
+            let b = 0.0;
 
-            let b = 0.25;
-            let pixel_color = Color::new(r, g, b);
-            color::write_color(&mut io::stdout(), pixel_color);
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
         }
     }
-
-    eprintln!("\nDone.");
 }
