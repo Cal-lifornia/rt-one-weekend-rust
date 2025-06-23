d10ccaa1bd4c3aa7042cfe5baeb78cd5282dd486
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-06-23 13:23:25 +1000

    On any linux os Windows will also be added as a target in case Bevy is being used

--- flake.nix
+++ flake.nix
@@ -40,17 +40,6 @@
               cargo-flamegraph
               gnuplot
               wgsl-analyzer
-              (rust-bin.selectLatestNightlyWith (
-                toolchain:
-                toolchain.default.override {
-                  extensions = [
-                    "rust-src"
-                    "rustfmt"
-                    "clippy"
-                    "llvm-tools"
-                  ];
-                }
-              ))
             ]
             # https://github.com/bevyengine/bevy/blob/v0.14.2/docs/linux_dependencies.md#nix
             ++ (lib.optionals pkgs.stdenv.isLinux [
@@ -63,10 +52,20 @@
               xorg.libXrandr # To use the x11 feature
               libxkbcommon
               wayland # To use the wayland feature
+              (rust-bin.stable.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rustfmt"
+                  "clippy"
+                  "llvm-tools"
+                ];
+                targets = [ "x86_64-pc-windows-gnu" ];
+              })
             ])
             ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
               # https://discourse.nixos.org/t/the-darwin-sdks-have-been-updated/55295/1
               apple-sdk_15
+              (rust-bin.stable.latest.default)
             ]);
           # LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
         };
--- win.sh
+++ win.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-cargo build --target x86_64-pc-windows-msvc &&
+cargo build --target x86_64-pc-windows-gnu &&
 cp target/x86_64-pc-windows-msvc/debug/rt-one-weekend.exe . &&
 exec ./rt-one-weekend.exe "$@"
